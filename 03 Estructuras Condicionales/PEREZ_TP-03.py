bbb62604bb80d21f68f3c751783ac93bcd0e0aff
lucasdperez96 <lucasdperez.96@gmail.com>
2025-08-31 18:52:30 -0300

    Update PEREZ_TP-03.py
    Commit de la actividad 3

--- 03 Estructuras Condicionales/PEREZ_TP-03.py
+++ 03 Estructuras Condicionales/PEREZ_TP-03.py
@@ -29,3 +29,14 @@ if nota >= aprobado :
     print ("Aprobado")
 else :
     print ("Desaprobado")
+
+# 3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+# número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+# contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+# operador de módulo (%) en Python para evaluar si un número es par o impar.
+
+numero = int(input("Ingrese un numero par: "))
+if numero % 2 == 0 :
+    print("Ha ingresado un número par")
+else :
+    print("Por favor, ingrese un número par")
