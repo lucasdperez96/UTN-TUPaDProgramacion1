7853ecafa2543410588af452d8a5da5dfcd9e73a
lucasdperez96 <lucasdperez.96@gmail.com>
2025-08-31 20:13:00 -0300

    Update PEREZ_TP-03.py
    commit consigna 9

--- 03 Estructuras Condicionales/PEREZ_TP-03.py
+++ 03 Estructuras Condicionales/PEREZ_TP-03.py
@@ -164,3 +164,30 @@ match valor :
         print(str.title(nombre))
     case _:
         print("Usted ingreso un valor distinto a 1, 2 o 3")
+
+# 9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+# magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+# por pantalla:
+# ● Menor que 3: "Muy leve" (imperceptible).
+# ● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+# ● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+# generalmente no causa daños).
+# ● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+# débiles).
+# ● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+# ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+
+magnitud = float(input("Ingrese magnitud de un terremoto: "))
+
+if magnitud < 3 :
+    print(""""Muy leve" (imperceptible).""")
+elif magnitud >= 3 and magnitud < 4 :
+    print(""""Leve" (ligeramente perceptible).""")
+elif magnitud >= 4 and magnitud < 5 :
+    print(""""Moderado" (sentido por personas, pero generalmente no causa daños).""")
+elif magnitud >= 4 and magnitud < 6 :
+    print(""""Fuerte" (puede causar daños en estructuras débiles).""")
+elif magnitud >= 6 and magnitud < 7 :
+    print(""""Muy Fuerte" (puede causar daños significativos).""")
+else :
+    print(""""Extremo" (puede causar graves daños a gran escala).""")
