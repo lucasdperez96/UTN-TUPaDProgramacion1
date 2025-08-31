e3d66e1158648bf3816a2b455ee4ae80efe54446
lucasdperez96 <lucasdperez.96@gmail.com>
2025-08-31 18:37:14 -0300

    Update PEREZ_TP-03.py
    Realizo la consigna 1

--- PEREZ_TP-03.py
+++ PEREZ_TP-03.py
@@ -5,3 +5,15 @@
 # Fecha de entrega: 18/08/2025
 # ===============================================
 
+# Actividades
+# 1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+# deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+
+edad = int(input("Ingrese su edad: "))
+mayoria_edad = int(18)
+if edad >= mayoria_edad:
+    print("Es mayor de edad")
+elif edad >= 0 or edad > 18: #Excede lo solicitado en la consigna pero es una mejora
+    print("Es menor de edad")
+else:
+    print("Valor ingreasado incorrecto")
