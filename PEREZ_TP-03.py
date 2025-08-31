feaebad136eccab21710c251fc1d35391bb972e9
lucasdperez96 <lucasdperez.96@gmail.com>
2025-08-31 18:36:22 -0300

    Create PEREZ_TP-03.py
    Creo el proyecto TP3

---
+++ PEREZ_TP-03.py
@@ -0,0 +1,7 @@
+# ===============================================
+# Trabajo Práctico 1: Estructuras secuenciales
+# Nombre y Apellido: Lucas Daniel Perez
+# DNI: 39656325
+# Fecha de entrega: 18/08/2025
+# ===============================================
+
