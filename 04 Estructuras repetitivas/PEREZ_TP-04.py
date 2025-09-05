44f9e6fb7b0f5d596c3e6cc96302eea6088fb51a
lucasdperez96 <lucasdperez.96@gmail.com>
2025-09-05 17:55:14 -0300

    Update PEREZ_TP-04.py
    commit ejercicio 4

--- 04 Estructuras repetitivas/PEREZ_TP-04.py
+++ 04 Estructuras repetitivas/PEREZ_TP-04.py
@@ -21,3 +21,33 @@ while num >= 10 :
     digitos+=1
 
 print("cantidad de digitos del numero ingresado: ", digitos)
+
+# 3) Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+# dados por el usuario, excluyendo esos dos valores.
+
+print("Suma de rangos numericos!\nEste programa suma todos los numeros comprendidos en un rango definido por usted (excluyendo las cotas).")
+ini = int(input("Ingrese numero inicial: "))
+fin = int(input("Ingrese numero final: "))
+strSuma=""
+suma = 0
+
+for x in range(ini+1,fin):
+    strSuma+=str(x) if x==ini+1 else " + " + str(x)
+    suma+=x
+    
+print(f"Resultado:\n{strSuma} = {suma}")
+
+# 4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
+# secuencia. El programa debe detenerse y mostrar el total acumulado cuando el usuario ingrese
+# un 0.
+
+print("Suma de enteros!\n-Ingrese los numeros uno a uno para sumar\n-Ingrese 0 para salir.")
+
+numero=int(input("ingrese un numero: ")) 
+suma=0
+while numero != 0 :
+    suma += numero
+    numero=int(input("ingrese un numero: "))
+    if numero == 0 :
+        break
+    print(f"{suma} + {numero} = {suma+numero}")
