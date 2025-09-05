21c775b74d85a2cb4e989f5336e5539883aea982
lucasdperez96 <lucasdperez.96@gmail.com>
2025-09-05 17:07:09 -0300

    Update PEREZ_TP-04.py
    commit ejercicio 1

--- 04 Estructuras repetitivas/PEREZ_TP-04.py
+++ 04 Estructuras repetitivas/PEREZ_TP-04.py
@@ -5,3 +5,8 @@
 # Fecha de entrega: 05/09/2025
 # ===============================================
 
+# 1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+# (incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+for x in range(101):
+    print(x)
