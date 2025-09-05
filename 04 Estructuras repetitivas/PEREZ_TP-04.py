af1ed76d7653f6ff86d02e580e1a65c25ce77a2a
lucasdperez96 <lucasdperez.96@gmail.com>
2025-09-05 17:03:59 -0300

    TP1 - Estructuras Secuenciales
    Carga inicial del archivo py

---
+++ 04 Estructuras repetitivas/PEREZ_TP-04.py
@@ -0,0 +1,7 @@
+# ===============================================
+# Trabajo Práctico 4: Estructuras repetitivas
+# Nombre y Apellido: Lucas Daniel Perez
+# DNI: 39656325
+# Fecha de entrega: 05/09/2025
+# ===============================================
+
